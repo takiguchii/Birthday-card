db7e3b621fb7fdea95bcd9a7a5e6fa7827713185
takiguchii <alexandre.pupim360@gmail.com>
2026-03-09 20:46:59 -0300

    Feat: Creating Initial Struct

---
+++ mybirthdaycart/android/app/src/main/kotlin/com/example/mybirthdaycart/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mybirthdaycart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ mybirthdaycart/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(birthCardApp());
+}
+
+class birthCardApp extends StatelessWidget {
+  const birthCardApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
